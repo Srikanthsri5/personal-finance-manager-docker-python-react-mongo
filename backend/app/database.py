6d644d32d97f82431a7d64c431211fadda1b859c
Srikanth P <p.srikanth@ahana.co.in>
2026-01-28 15:54:03 +0530

    feat: Implement initial backend and frontend structure for expense management with Dockerization and updated port configuration.

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb://localhost:27017/pfm_db")
+MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://srikanthsri051199:2KcU3HEsjTGDQeD5@personal-finance.r95w4we.mongodb.net/pfm_db")
 
 client = AsyncIOMotorClient(MONGODB_URL)
 db = client.get_database()
